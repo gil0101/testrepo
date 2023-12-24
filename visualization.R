c69579205d147f84be024850091b2dc1d26a7180
gil0101 <adjahossougildas@gmail.com>
2023-12-24 10:07:49 +0100

    visualization data with ggplot2 library in R

---
+++ visualization.R
@@ -0,0 +1,54 @@
+library(datasets)
+
+#load Data
+
+data(mtcars)
+
+#View first 5 rows
+
+head(mtcars,5)
+?mtcars
+######################without title
+#load ggplot packagr
+library(ggplot2)
+#create a scatterplot of disp and mgp
+ggplot(aes(x=disp, y=mpg,),data=mtcars)+geom_point()
+
+######################with title
+#load ggplot packagr
+library(ggplot2)
+#create a scatterplot of disp and mgp
+ggplot(aes(x=disp, y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon")
+
+############with title+ labeling axis
+#change axis name
+
+ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon") + labs(x = "Displacement", y = "Miles per Gallon")
+
+#make vs a factor
+mtcars$vs <- as.factor(mtcars$vs)
+
+#create boxplot of the distribution for v-shaped and straight Engine
+
+ggplot(aes(x=vs, y=mpg), data = mtcars) + geom_boxplot()
+
+ggplot(aes(x=vs, y=mpg, fill = vs), data = mtcars) + 
+  geom_boxplot(alpha=0.3) +
+  theme(legend.position="none")
+
+ggplot(aes(x=wt),data=mtcars) + geom_histogram(binwidth=0.8)
+
+
+
+
+
+
+
+
+
+
+library(datasets)
+data(iris)
+
+library(GGally)
+ggpairs(iris, mapping=ggplot2::aes(colour = Species))
